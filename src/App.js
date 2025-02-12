bf9871a2fec586e87f7ec0c860b9d11da9899307
Emilio Mendoza <emilio2601@gmail.com>
2025-02-12 15:15:09 -0500

    refactor bullet, show diff size if diamond

--- src/App.js
+++ src/App.js
@@ -327,17 +327,27 @@ const MTASubway = () => {
 
 const MTASubwayBullet = ({ route, size }) => {
   const transformRouteName = () => {
-    if (route == "6X" || route == "7X") {
+    if (route == "6X" || route == "7X" || route == "FX") {
       return route.replace("X", "d")
     }
 
     return route?.toLowerCase()
   }
 
-  const customClass = size == "sm" ? "w-8 h-8" : "w-32 h-32"
+  const getClass = (route, size) => {
+    if (size == "sm" && route?.endsWith("X")) {
+      return "w-10 h-10"
+    } else if (size == "sm") {
+      return "w-8 h-8"
+    } else if (route?.endsWith("X")) {
+      return "w-36 h-36"
+    } {
+      return "w-32 h-32"
+    }
+  }
 
   return (
-    <img src={`https://unpkg.com/mta-subway-bullets@0.5.0/svg/${transformRouteName()}.svg`} className={`rounded-full inline px-1 mb-1 ${customClass}`}/>
+    <img src={`/svg/${transformRouteName()}.svg`} className={`rounded-full inline px-1 mb-1 ${getClass(route, size)}`}/>
   )
 }
 
